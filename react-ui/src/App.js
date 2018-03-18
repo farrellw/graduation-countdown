726e04200d42a07007b7991fe33ac3d47d1db7e4
farrellw <farrell.will.g@gmail.com>
2018-03-18 17:57:03 -0500

    Add a component, check if it can build to heroku with proper jsx file extension.

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CountdownContainer from "./components/CountdownContainer.jsx";
 
 class App extends Component {
   render() {
@@ -8,11 +9,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Brown School Graduation 2018</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <CountdownContainer />
       </div>
     );
   }
---
+++ react-ui/src/components/CountdownContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+var ingredients = [{"id": 1, "text": "ham"}, {"id": 2, "text": "cheese"}, {"id": 3, "text": "potatoes"}];
+
+export default class CountdownContainer extends React.Component {
+  render(){
+    return (
+      <h3>This is where the container would go</h3>
+    );
+  }
+}
