2905e9d9c0c2cc7af7812495b8eb65e5105ebbca
farrellw <farrell.will.g@gmail.com>
2018-03-18 18:36:07 -0500

    Add countdown to brown school graduation. Add css for the countdown.

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -9,11 +9,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Brown School Graduation 2018</h1>
+          <h1 className="App-title">Brown School Graduation 2018</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <CountdownContainer />
       </div>
     );
--- react-ui/src/components/CountdownContainer.jsx
+++ react-ui/src/components/CountdownContainer.jsx
@@ -1,12 +1,66 @@
 import React from "react";
-
-
-var ingredients = [{"id": 1, "text": "ham"}, {"id": 2, "text": "cheese"}, {"id": 3, "text": "potatoes"}];
+import './countdown.css';
 
 export default class CountdownContainer extends React.Component {
+  constructor() {
+		super();
+    this.graduationDate = new Date('May 17, 2018 23:30:00');
+    this.state = {
+			timeLeft: {
+				days: 0,
+				hours: 0,
+				minutes: 0,
+				seconds: 0
+			}
+		};
+	}
+
+	calculateTimeLeft(){
+		var now = new Date();
+		var difference = this.graduationDate - now;
+
+    var days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+		this.setState({ timeLeft: {
+			hours: hours,
+			days: days,
+			minutes: minutes,
+			seconds: seconds
+		}});
+	}
+
+  componentDidMount(){
+    this.calculateTimeLeft();
+
+    var self = this;
+    setInterval(function() {
+      self.calculateTimeLeft();
+    }, 1000);
+  }
+
   render(){
     return (
-      <h3>This is where the container would go</h3>
+      <div id="countdown">
+        <div id="tiles">
+          <span>{this.state.timeLeft.days}</span>
+          <span>{this.state.timeLeft.hours}</span>
+          <span>{this.state.timeLeft.minutes}</span>
+          <span>{this.state.timeLeft.seconds}</span>
+        </div>
+        <div className="labels">
+    			<li>Days
+    			</li>
+    			<li>Hours
+    			</li>
+    			<li>Minutes
+    			</li>
+    			<li>Seconds
+    			</li>
+    		</div>
+      </div>
     );
   }
 }
