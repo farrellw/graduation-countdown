7d67e9be502c256ba5d40780980747e3654b6b0a
farrellw <farrell.will.g@gmail.com>
2018-07-30 20:34:02 -0500

    Change to october.

--- react-ui/src/data/countdown/CountdownStore.js
+++ react-ui/src/data/countdown/CountdownStore.js
@@ -1,27 +1,27 @@
-import {ReduceStore} from 'flux/utils';
-import CountdownActionTypes from './CountdownActionTypes';
-import Dispatcher from '../Dispatcher';
+import { ReduceStore } from "flux/utils";
+import CountdownActionTypes from "./CountdownActionTypes";
+import Dispatcher from "../Dispatcher";
 
 class CountdownStore extends ReduceStore {
-	constructor(){
-		super(Dispatcher);
-	}
+  constructor() {
+    super(Dispatcher);
+  }
 
-	getInitialState(){
-		return new Date('May 17, 2018 19:30:00');
-	}
+  getInitialState() {
+    return new Date("Oct 17, 2018 19:30:00");
+  }
 
-	reduce(state, action){
-		switch(action.type){
-			case CountdownActionTypes.UPDATE_DATE:
-				if(!action.date){
-					return state;
-				}
-				return state.set(action.date);
-			default:
-				return state;
-		}
-	}
+  reduce(state, action) {
+    switch (action.type) {
+      case CountdownActionTypes.UPDATE_DATE:
+        if (!action.date) {
+          return state;
+        }
+        return state.set(action.date);
+      default:
+        return state;
+    }
+  }
 }
 
 export default new CountdownStore();
